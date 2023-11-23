58f257b6d203fbaab6181780e485472de569f93b
sehwanii <sehwan1023@gmail.com>
2023-11-24 01:10:16 +0900

    add secretkey

--- django/config/settings.py
+++ django/config/settings.py
@@ -9,18 +9,34 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import json
+import os
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&c-@p@4(si$6__@+-hh=%asq=f_9%1a8!44&($ewe#mj4p1i+h'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
